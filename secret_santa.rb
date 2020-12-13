32bc0ee87596fbe83e9ff734a3273e7d464f60b4
Meagan Waller <meaganewaller@gmail.com>
2020-12-12 22:07:04 -0500

    ✨ Create potential santas
    For each person in the secret santa list, create an Array of who they
    could potentially be matched up to, ensuring no one from their family
    is included.

--- secret_santa.rb
+++ secret_santa.rb
@@ -1,7 +1,15 @@
+require 'pry'
+require 'pry-nav'
 class SecretSanta
   attr_accessor :santas
 
   def initialize(file)
-    @santas = File.open(file).read.split("\n")
+    @santas = File.open(file).read.split("\n").map { |s| s.split }
+  end
+
+  def potential_santas
+    @santas.map do |santa|
+      { "#{santa[0]} #{santa[1]}" => @santas.reject { |s| s[1] == santap[1] } }
+    end
   end
 end
--- spec/secret_santa_spec.rb
+++ spec/secret_santa_spec.rb
@@ -6,4 +6,9 @@ describe SecretSanta do
     secret_santa = SecretSanta.new('./santas.txt')
     expect(secret_santa.santas).to_not be_empty
   end
+
+  it "doesn't allow someone to be assigned to their family member" do
+    secret_santa = SecretSanta.new('./santas.txt')
+    expect(secret_santa.potential_santas[0]["Alice Jones"]).to eql [["Demi", "Smith", "<demi@example.com>"], ["Eli", "Smith", "<eli@example.com>"], ["Florence", "Williams", "<florence@example.com>"]]
+  end
 end
